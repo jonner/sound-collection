838647f08228015d94a229ff77457cf87eb62f70
Jonathon Jongsma <jjongsma@redhat.com>
2014-05-28 17:34:47 -0500

    Add 'quality' column to recording tree view

--- src/recording-tree-view.cc
+++ src/recording-tree-view.cc
@@ -31,6 +31,17 @@ static void duration_data_func(Gtk::CellRenderer* renderer, const Gtk::TreeModel
     crt->property_text() = format_duration(duration);
 }
 
+static void quality_data_func(Gtk::CellRenderer* renderer, const Gtk::TreeModel::const_iterator& iter, const Glib::RefPtr<RecordingTreeModel>& model, int q)
+{
+    float quality = iter->get_value(model->columns().quality);
+    Gtk::CellRendererPixbuf* crp = dynamic_cast<Gtk::CellRendererPixbuf*>(renderer);
+
+    if (!quality)
+        crp->property_icon_name() = "";
+    else
+        crp->property_icon_name() = quality >= q ? "starred" : "non-starred";
+}
+
 struct RecordingTreeView::Priv {
     Glib::RefPtr<RecordingTreeModel> model;
     Gtk::TreeViewColumn id;
@@ -38,11 +49,18 @@ struct RecordingTreeView::Priv {
     Gtk::CellRendererText file_renderer;
     Gtk::CellRendererText duration_renderer;
     Gtk::TreeViewColumn duration;
+    Gtk::CellRendererPixbuf quality_renderer1;
+    Gtk::CellRendererPixbuf quality_renderer2;
+    Gtk::CellRendererPixbuf quality_renderer3;
+    Gtk::CellRendererPixbuf quality_renderer4;
+    Gtk::CellRendererPixbuf quality_renderer5;
+    Gtk::TreeViewColumn quality;
 
     Priv()
         : id("ID")
         , file("File")
         , duration("Duration")
+        , quality("Quality")
     {
         id.set_sizing(Gtk::TREE_VIEW_COLUMN_FIXED);
         id.set_fixed_width(40);
@@ -54,6 +72,15 @@ struct RecordingTreeView::Priv {
         duration.set_sizing(Gtk::TREE_VIEW_COLUMN_FIXED);
         duration.set_fixed_width(100);
         duration.set_resizable(true);
+        quality.set_sizing(Gtk::TREE_VIEW_COLUMN_FIXED);
+        quality.set_fixed_width(100);
+        quality.set_resizable(true);
+
+        quality_renderer1.set_alignment(0.0, 0.5);
+        quality_renderer2.set_alignment(0.0, 0.5);
+        quality_renderer3.set_alignment(0.0, 0.5);
+        quality_renderer4.set_alignment(0.0, 0.5);
+        quality_renderer5.set_alignment(0.0, 0.5);
     }
 };
 
@@ -64,6 +91,7 @@ RecordingTreeView::RecordingTreeView()
     append_column(m_priv->id);
     append_column(m_priv->file);
     append_column(m_priv->duration);
+    append_column(m_priv->quality);
 }
 
 void RecordingTreeView::set_model(const Glib::RefPtr<RecordingTreeModel>& model)
@@ -84,5 +112,15 @@ void RecordingTreeView::set_model(const Glib::RefPtr<RecordingTreeModel>& model)
     m_priv->duration.pack_start(m_priv->duration_renderer);
     m_priv->duration.set_renderer(m_priv->duration_renderer, model->columns().duration);
     m_priv->duration.set_cell_data_func(m_priv->duration_renderer, sigc::bind(sigc::ptr_fun(&duration_data_func), sigc::ref(model)));
+    m_priv->quality.pack_start(m_priv->quality_renderer1, false);
+    m_priv->quality.pack_start(m_priv->quality_renderer2, false);
+    m_priv->quality.pack_start(m_priv->quality_renderer3, false);
+    m_priv->quality.pack_start(m_priv->quality_renderer4, false);
+    m_priv->quality.pack_start(m_priv->quality_renderer5, false);
+    m_priv->quality.set_cell_data_func(m_priv->quality_renderer1, sigc::bind(sigc::ptr_fun(&quality_data_func), sigc::ref(model), 1));
+    m_priv->quality.set_cell_data_func(m_priv->quality_renderer2, sigc::bind(sigc::ptr_fun(&quality_data_func), sigc::ref(model), 2));
+    m_priv->quality.set_cell_data_func(m_priv->quality_renderer3, sigc::bind(sigc::ptr_fun(&quality_data_func), sigc::ref(model), 3));
+    m_priv->quality.set_cell_data_func(m_priv->quality_renderer4, sigc::bind(sigc::ptr_fun(&quality_data_func), sigc::ref(model), 4));
+    m_priv->quality.set_cell_data_func(m_priv->quality_renderer5, sigc::bind(sigc::ptr_fun(&quality_data_func), sigc::ref(model), 5));
 }
 }
